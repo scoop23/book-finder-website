7b824d4ff95bd2943fb4d59bf0e18ff61e5c0ef5
Sebasch Amauri Santillan <sebaschamauri.09@gmail.com>
2025-06-11 14:28:38 +0800

    fixed the searchByTitle functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { FaSearch , FaHome } from 'react-icons/fa';
 import { animate } from 'motion';
 import { useElementScroll } from 'motion/react';
 
-
 const SearchBar = ({ value , onValueChange, setSearchType }) => {  
+  const [clickedSearchTitle, setClickedSearchTitle] = useState(false);
+  const [clickedSearchAuthor, setClickedSearchAuthor] = useState(false);
+
   const searchTitle = useRef();
   function handleClickSearch() {
     const input = document.querySelector('.input-search');
@@ -20,7 +22,7 @@ const SearchBar = ({ value , onValueChange, setSearchType }) => {
       })
     }
   }
-  
+
 function buttonSearchTitle() {
   const element = searchTitle.current;
 
@@ -29,16 +31,17 @@ function buttonSearchTitle() {
     return;
   }
 
-  const isSelected = element.classList.contains("hover:bg-amber-50");
 
-  if (isSelected) {
+  if (!clickedSearchTitle) {
     setSearchType("title");
-    element.classList.remove("hover:bg-amber-50");
-    element.classList.add("bg-amber-50", "text-black", "hover:bg-primary-blackrock");
+    setClickedSearchTitle(true)
+    element.classList.remove("hover:bg-amber-50", "hover:text-black");
+    element.classList.add("bg-amber-50", "text-black", "hover:bg-primary-ebony-clay", "hover:text-amber-100");
   } else {
     setSearchType("");
-    element.classList.remove("bg-amber-50", "text-black", "hover:bg-primary-blackrock");
-    element.classList.add("hover:bg-amber-50");
+    setClickedSearchTitle(false)
+    element.classList.remove("bg-amber-50","text-black", "hover:bg-primary-ebony-clay", "hover:bg-amber-50");
+    element.classList.add("hover:bg-amber-50" , "text-amber-100");
   }
 }
 
@@ -48,13 +51,16 @@ function buttonSearchTitle() {
       <div className='outer-search-bar-container p-[20px] max-w-full min-h-[20px] flex justify-between gap-2 '>
         <button className='home-button flex flex-row gap-2 items-center rounded-4xl p-8 border-1 h-[70px] text-amber-100 hover:text-black hover:bg-amber-50 duration-250 transition-all cursor-pointer'><FaHome /></button>
         <div className='flex gap-2'>
+
           <button className='search-title hover:bg-amber-50 duration-250 transition-all flex justify-center items-center border-1 rounded-4xl p-8 h-[70px] font-winky cursor-pointer hover:shadow-lg hover:text-black text-amber-100'
           onClick={() => buttonSearchTitle()} ref={searchTitle}>Search By Title</button>
+
           <button className='search-author hover:bg-amber-50 duration-250 transition-all flex justify-center items-center border-1 rounded-4xl p-8 h-[70px] font-winky cursor-pointer hover:shadow-lg hover:text-black text-amber-100'
           onClick={() => setSearchType("author")}>Search by Author</button>
-            <div className='inner-search flex flex-row gap-2 items-center rounded-4xl p-3 bg-amber-50 h-[70px]'>
+
+            <div className='inner-search flex flex-row gap-2 items-center rounded-4xl p-4 bg-amber-50 h-[70px]'>
               <input
-              className={`input-search hidden w-0 rounded-2xl outline-0 font-inter`}
+              className={`input-search hidden w-0 rounded-2xl outline-0 font-inter p-4`}
               type="text"
               defaultValue={""}
               onKeyDown={(e) => {
