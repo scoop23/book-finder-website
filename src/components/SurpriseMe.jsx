037758424a9508f3503970d0ac8d0aa33074f40b
Sebasch Amauri Santillan <sebaschamauri.09@gmail.com>
2025-07-06 02:07:21 +0800

    init the surpriseme.jsx

---
+++ src/components/SurpriseMe.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SurpriseMe = () => {
+  return (
+    <div className='surprise-me-wrapper'>
+      <div className='surprise-me-main flex justify-center items-center h-full'>
+        <div className='surprise-me bg-zinc-900 w-[200px] h-[200px] rounded-2xl border-1 border-zinc-400'>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SurpriseMe;
