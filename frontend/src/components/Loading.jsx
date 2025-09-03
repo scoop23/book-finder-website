52223d33ec30845b115a63e0a776bf3dbfb877aa
Sebasch Amauri Santillan <sebaschamauri.09@gmail.com>
2025-09-03 23:20:40 +0800

    centered the loading svg

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import SpinnerSVG from './icons/SpinnerSVG.jsx'
 
 const Loading = () => {
-  return (
+
+  return ( // fixed, inset-0 - fixed relative to the viewport no the parent basically left  = 0 ,right =0 ,top =0 , bottom =0 then center it with flex box.
     <div className='fixed inset-0 flex items-center justify-center w-full h-full'>
       <SpinnerSVG className='w-30 h-30'/>
     </div>
