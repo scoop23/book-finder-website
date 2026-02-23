495170aff2ccb3a28520fc168c87da1aa9370809
Sebasch Amauri Santillan <sebaschamauri.09@gmail.com>
2026-02-23 13:57:34 +0800

    WIP: fixing animation of the modals

--- frontend/src/components/BookModal/BookModalContent.jsx
+++ frontend/src/components/BookModal/BookModalContent.jsx
@@ -8,29 +8,36 @@ const BookModalContent = ({ workData, isModal }) => {
     }
   }
 
+  const containerVariant = {
+    hidden: { scale: 0.8, opacity: 0 },
+    visible: { scale: 1, opacity: 1, transition: { duration: 0.5, type: "spring" } },
+    exit: { scale: 0.7, opacity: 0 }
+  }
+
   return (
     <motion.div className="modal-content flex flex-row w-[1200px] h-[800px] p-4">
-      <motion.div className="primary-container min-w-[700px] h-full rounded-2xl flex flex-col gap-4 justify-center items-center"> 
-        <motion.div className="book-content p-4 h-[400px] w-[700px] bg-white rounded-2xl"> {/* content here? */} 
-        </motion.div> 
-          
-        <motion.div className="flex flex-row gap-2 rounded-2xl"> 
-          <motion.div className="additions bg-red-500 w-[350px] h-[250px] rounded-2xl">
+      <motion.div className="primary-container min-w-[700px] h-full rounded-2xl flex flex-col gap-4 justify-baseline items-center">
+        <motion.div layout variants={containerVariant} initial={"hidden"} animate={"visible"} exit={"exit"} className="book-content p-4 h-[400px] w-[700px] bg-white rounded-2xl">
+          {/* content here? */}
+        </motion.div>
+
+        <motion.div className="flex flex-row gap-2 rounded-2xl">
+          <motion.div className="additions bg-white w-[350px] h-[250px] rounded-2xl">
 
           </motion.div>
-          <motion.div className="additions bg-blue-500 w-[350px] h-[250px] rounded-2xl" >
+          <motion.div className="additions bg-white w-[350px] h-[250px] rounded-2xl" >
 
           </motion.div>
         </motion.div>
       </motion.div>
 
-      <motion.div className="secondary-container flex flex-col p-4">
+      <motion.div className="secondary-container flex flex-col px-4 mt-8 w-full h-full gap-4 items-center">
         {
-          [1,2,3,4,5].map(n => {
+          [1, 2, 3, 4, 5].map(n => {
             return (
-              <div className="bg-white ">
-              {n}
-              </div>
+              <motion.div className="bg-white w-full rounded-2xl px-4">
+                {n}
+              </motion.div>
             )
           })
         }
